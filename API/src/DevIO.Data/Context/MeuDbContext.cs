cabddff0d362b7d41258fcc98b278ec93ecc4f16
Mauricio Hartmann <mauricio.hartmann@outlook.com>
2021-12-25 14:52:04 -0300

    Criado DbContext

---
+++ API/src/DevIO.Data/Context/MeuDbContext.cs
@@ -0,0 +1,56 @@
+﻿using DevIO.Business.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DevIO.Data.Context
+{
+    public class MeuDbContext : DbContext
+    {
+        public MeuDbContext(DbContextOptions<MeuDbContext> options) : base(options) { }
+
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Endereco> Enderecos { get; set; }
+        public DbSet<Fornecedor> Fornecedores { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Propriedades no formato string que não possuirem definições explicitas ficam como VARCHAR(100)
+            var properties = modelBuilder.Model.GetEntityTypes()
+                .SelectMany(e => e.GetProperties()
+                .Where(p => p.ClrType == typeof(string)));
+            foreach (var property in properties)
+            {
+                property.SetColumnType("varchar(100)");
+            }
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(MeuDbContext).Assembly);
+            // Configura o comportamento do banco para deleção de entidades
+            var relations = modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys());
+            foreach (var relationship in relations)
+            {
+                relationship.DeleteBehavior = DeleteBehavior.ClientSetNull;
+            }
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+        {
+            var entries = ChangeTracker.Entries()
+                .Where(entry => entry.Entity.GetType().GetProperty("DataCadastro") != null);
+            foreach (var entry in entries)
+            {
+                if (entry.State == EntityState.Added)
+                {
+                    entry.Property("DataCadastro").CurrentValue = DateTime.Now;
+                }
+                if (entry.State == EntityState.Modified)
+                {
+                    entry.Property("DataCadastro").IsModified = false;
+                }
+            }
+            return base.SaveChangesAsync();
+        }
+    }
+}
