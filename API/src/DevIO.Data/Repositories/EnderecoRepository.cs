2ab5e32f4bec5348c5b802b375847cb08d961530
Mauricio Hartmann <mauricio.hartmann@outlook.com>
2021-12-25 14:53:07 -0300

    Repositórios
    - Criada classe base dos repositórios
    - Criados repositórios das entidades

---
+++ API/src/DevIO.Data/Repositories/EnderecoRepository.cs
@@ -0,0 +1,18 @@
+﻿using DevIO.Business.Models;
+using DevIO.Data.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Threading.Tasks;
+
+namespace DevIO.Data.Repositories
+{
+    public class EnderecoRepository : Repository<Endereco>
+    {
+        public EnderecoRepository(MeuDbContext context) : base(context) { }
+
+        public async Task<Endereco> ObterEnderecoPorFornecedor(Guid fornecedorId)
+        {
+            return await Db.Enderecos.AsNoTracking().FirstOrDefaultAsync(f => f.FornecedorId == fornecedorId);
+        }
+    }
+}
