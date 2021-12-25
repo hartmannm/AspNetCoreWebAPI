956ba6fa9d85e884b53baaeceacc082100b9bac9
Mauricio Hartmann <mauricio.hartmann@outlook.com>
2021-12-25 14:03:39 -0300

    Interfaces
    - Criada interface de usuário
    - Criadas interfaces dos repositórios
    - Criadas interfaces dos sevices

---
+++ API/DevIO.Business/Interfaces/Repositories/IEnderecoRepository.cs
@@ -0,0 +1,11 @@
+﻿using DevIO.Business.Models;
+using System;
+using System.Threading.Tasks;
+
+namespace DevIO.Business.Interfaces.Repositories
+{
+    public interface IEnderecoRepository : IRepository<Endereco>
+    {
+        Task<Endereco> ObterEnderecoPorFornecedor(Guid fornecedorId);
+    }
+}
