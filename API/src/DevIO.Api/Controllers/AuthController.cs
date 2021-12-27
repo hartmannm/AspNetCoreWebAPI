df79021caba646bd424e3552b51b00d874222b63
Mauricio Hartmann <mauricio.hartmann@outlook.com>
2021-12-27 20:26:03 -0300

    Finalização da autenticação
    - Criados models para transferência de dados de usuário logado
    - Ajuste no login para retornar no novo model

--- API/src/DevIO.Api/Controllers/AuthController.cs
+++ API/src/DevIO.Api/Controllers/AuthController.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
 using System;
 using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
 using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
@@ -69,7 +70,7 @@ namespace DevIO.Api.Controllers
             return CustomResponse(loginUser);
         }
 
-        private async Task<string> GerarJwt(string email)
+        private async Task<LoginResponseViewModel> GerarJwt(string email)
         {
             var user = await _userManager.FindByEmailAsync(email);
             var claims = await _userManager.GetClaimsAsync(user);
@@ -99,7 +100,17 @@ namespace DevIO.Api.Controllers
                 SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256)
             });
             var encodedToken = tokenHandler.WriteToken(token);
-            return encodedToken;
+            return new LoginResponseViewModel
+            {
+                AccessToken = encodedToken,
+                ExpiresIn = TimeSpan.FromHours(_appSettings.ExpiracaoHoras).TotalSeconds,
+                UserToken = new UserTokenViewModel
+                {
+                    Id = user.Id,
+                    Email = user.Email,
+                    Claims = claims.Select(c => new ClaimViewModel { Type = c.Type, Value = c.Value })
+                }
+            };
         }
 
         private static long ToUnixEpochDate(DateTime date)
