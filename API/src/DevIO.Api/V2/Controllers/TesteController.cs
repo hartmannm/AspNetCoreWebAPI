490648fdbd3770baac60ab55469e54446176dcf5
Mauricio Hartmann <mauricio.hartmann@outlook.com>
2021-12-28 14:53:51 -0300

    Teste de versionamento
    Criados controllers para teste com versionamento V1 eV2

---
+++ API/src/DevIO.Api/V2/Controllers/TesteController.cs
@@ -0,0 +1,16 @@
+﻿using DevIO.Api.Controllers;
+using DevIO.Business.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevIO.Api.V2.Controllers
+{
+    [ApiVersion("2.0")]
+    [Route("api/v{version:ApiVersion}/teste")]
+    public class TesteController : MainController
+    {
+        public TesteController(INotificador notificador, IUser appUser) : base(notificador, appUser) { }
+
+        [HttpGet]
+        public string Valor() => "Sou a V2";
+    }
+}
